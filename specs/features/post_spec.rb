447b1edb6852e95e941e357116ae51e52c7a72ff
Johnson Joseph <jhonsonj88@gmail.com>
2019-06-16 19:42:54 +0000

    Automatically backed up by Learn

--- specs/features/post_spec.rb
+++ specs/features/post_spec.rb
@@ -5,4 +5,14 @@ describe 'new post' do
     visit new_post_path
     expect(page.status_code).to eq(200)
   end
+
+  describe 'new post' do
+
+  ...
+
+  it 'renders HTML in the /new template' do
+    visit new_post_path
+    expect(page).to have_content('Post Form')
+  end
+ end
 end
